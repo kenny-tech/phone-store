5932a3caff7621c647b810b6d49f9bc27ec8ea28
kenny-tech <kennyemma2008@gmail.com>
2020-07-22 14:18:29 +0100

    Created ProductList Component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Row, Col, Card, Button } from 'react-bootstrap';
+
+import { storeProducts } from '../data';
+import '../css/Product.css';
 
 const ProductList = () => {
     return (
         <Row>
-            <Col md={3} className="m-3">
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Product Title<span class="float-right">$20</span></Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary" className="btn-sm">Add To Cart</Button>
-                    </Card.Body>
-                </Card>
-            </Col>
+            {storeProducts.map(product => {
+                return (
+                    <Col md={3} className="m-2" key={product.id}>
+                        <Card className="productCard">
+                            <Card.Img variant="top" className="productImage" src={product.img} />
+                            <Card.Body>
+                                <Card.Title>{product.title}<span className="float-right">${product.price}</span></Card.Title>
+                                <Card.Text>
+                                    {product.short_description}
+                                </Card.Text>
+                                <Button variant="primary" className="btn-sm">Add To Cart</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                )
+            })}
         </Row>
     )
 }
