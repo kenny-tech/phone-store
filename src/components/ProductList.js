6e8bf9c3158e6ae82cd40f8126df35aaf0f3e463
kenny-tech <kennyemma2008@gmail.com>
2020-08-11 13:26:40 +0100

    Created product modal

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,29 +1,54 @@
-import React from 'react';
-import { Row, Col, Card, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Row, Col, Card, Button, Modal } from 'react-bootstrap';
 
 import { storeProducts } from '../data';
 import '../css/Product.css';
 
 const ProductList = () => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <Row>
-            {storeProducts.map(product => {
-                return (
-                    <Col md={3} className="m-2" key={product.id}>
-                        <Card className="productCard">
-                            <Card.Img variant="top" className="productImage" src={product.img} />
-                            <Card.Body>
-                                <Card.Title>{product.title}<span className="float-right">${product.price}</span></Card.Title>
-                                <Card.Text>
-                                    {product.short_description}
-                                </Card.Text>
-                                <Button variant="primary" className="btn-sm">Add To Cart</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                )
-            })}
-        </Row>
+        <React.Fragment>
+            <Row>
+                {storeProducts.map(product => {
+                    return (
+                        <Col md={3} className="m-2" key={product.id}>
+                            <Card className="productCard">
+                                <Card.Img variant="top" className="productImage" src={product.img} />
+                                <Card.Body>
+                                    <Card.Title>{product.title}<span className="float-right">${product.price}</span></Card.Title>
+                                    <Card.Text>
+                                        {product.short_description}
+                                    </Card.Text>
+                                    <Button variant="primary" onClick={handleShow}>
+                                        View Details
+                                    </Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )
+                })}
+            </Row>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </React.Fragment>
+        
     )
 }
 
